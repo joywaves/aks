49464412bea1d02f507a7a4f9a90786f6e4659e0
Peter Chaploutskii <pchaploutskii@axisgroup.com>
2021-07-12 17:02:34 -0400

    update filename

--- windows-exporter-setup.bat
+++ windows-exporter-setup.bat
@@ -1,4 +1,4 @@
 mkdir -p C:\aks
 cd C:\aks
 curl -LO https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi
-msiexec /i C:\aks\windows_exporter-0.13.0-amd64.msi /quiet /q /norestart /log c:\aks\install.log ENABLED_COLLECTORS=os,iis,cpu,system,container,logical_disk,net,cs,memory,terminal_services,tcp LISTEN_PORT=9100
+msiexec /i C:\aks\windows_exporter-0.16.0-amd64.msi /quiet /q /norestart /log c:\aks\install.log ENABLED_COLLECTORS=os,iis,cpu,system,container,logical_disk,net,cs,memory,terminal_services,tcp LISTEN_PORT=9100
